a352c36fa71195b9aea1534dba788032caa47e7f
Lucía Fresno <lucia.fresno.olmeda@gmail.com>
2024-02-12 11:01:23 +0100

    Sección 11. Publicación de fotos en la API implementada (en el cliente aún no).

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -2,6 +2,7 @@
 using API.Data;
 using API.DTOs;
 using API.Entities;
+using API.Extensions;
 using API.Interfaces;
 using AutoMapper;
 using Microsoft.AspNetCore.Authorization;
@@ -15,16 +16,18 @@ public class UsersController : BaseApiController
 {
     private readonly IUserRepository _userRepository;
     private readonly IMapper _mapper;
+    private readonly IPhotoService _photoService;
 
     //constructor
-    public UsersController(IUserRepository userRepository, IMapper mapper)
+    public UsersController(IUserRepository userRepository, IMapper mapper, IPhotoService photoService)
     {
         _userRepository = userRepository;
         _mapper = mapper;
+        _photoService = photoService;
     }
 
     [AllowAnonymous]
-    [HttpGet]   //al hacer un http get se hace el siguiente método: GetUser()
+    [HttpGet]   //al hacer un http get se hace el siguiente método: GetUser(). SI TODO SALE BN SE DEVUELVE Ok();
     // public  ActionResult<IEnumerable<AppUser>> GetUsers() => MÉTODO SÍNCRONO
     public async Task<ActionResult<IEnumerable<MemberDto>>> GetUsers() //MÉTODO ASÍNCRONO (no queremos bloquear la app)
     {
@@ -53,10 +56,10 @@ public class UsersController : BaseApiController
         return await _userRepository.GetMemberAsync(username);
     }
 
-    [HttpPut]
+    [HttpPut]   //se todo ha salido bn, debe devolver NoContent();
     public async Task<ActionResult> UpdateUser(MemberUpdateDto memberUpdateDto)
     {
-        var username = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+        var username = User.GetUsername();  //se ha definido esta operación en ClaimsPrincipalExtension
         var user = await _userRepository.GetUserByUsernameAsync(username);
 
         if(user == null) return NotFound();
@@ -70,6 +73,41 @@ public class UsersController : BaseApiController
         //si no se han hecho cambios, tendremos esta respuesta
         return BadRequest("Failed to update user");
 
+    }
+
+    //le añadimos una ruta porque ya tenemos otro httpPost (para diferenciarlo)
+    [HttpPost("add-photo")] //si todo sale bien, hay que devolver 201, Created
+    public async Task<ActionResult<PhotoDto>> AddPhoto(IFormFile file)
+    {
+        var user = await _userRepository.GetUserByUsernameAsync(User.GetUsername());
+
+        if(user == null) return NotFound();
+
+        var result = await _photoService.AddPhotoAsync(file);
+
+        if(result.Error != null) return BadRequest(result.Error.Message);
+
+        var photo = new Photo
+        {
+            Url = result.SecureUrl.AbsoluteUri,
+            PublicId = result.PublicId
+        };
+
+        if(user.Photos.Count == 0) photo.IsMain = true; //si es la priemra foto que se sube, se pone como perfil
+
+        user.Photos.Add(photo);
+
+        if(await _userRepository.SaveAllAsync())
+        {
+            //return _mapper.Map<PhotoDto>(photo);  //Esto funciona, pero devuelve 200 OK y queremos devolver 201 Created 
+                                                    // junto con la posición del lugar donde se ha guardado el recurso
+            
+            //el segundo parámetro se debe pasar como un objeto
+            return CreatedAtAction(nameof(GetUser), new {username = user.UserName}, _mapper.Map<PhotoDto>(photo));
+            //esto devuelve 201 created y dónde se ha creado el recurso (lo que tiene que devolver HttpPost por convenio)
+        }
+
+        return BadRequest("Problem adding photo");
     }
 
 }
--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -1,6 +1,7 @@
 
 
 using API.Data;
+using API.Helpers;
 using API.Interfaces;
 using API.Services;
 using Microsoft.EntityFrameworkCore;
@@ -24,6 +25,10 @@ namespace API.Extensions
             services.AddScoped<IUserRepository, UserRepository>();
 
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+            services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
+
+            services.AddScoped<IPhotoService, PhotoService>();
             return services;
         }
 
